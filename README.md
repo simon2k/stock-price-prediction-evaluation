9f0ea55f6f5523873386959f32f84dceaa0c0067
Szymon Kieloch <szymon@sk-solutions.pl>
2019-09-10 21:34:34 +0200

    Compare more models using the same settings
    Base settings
    - Alpha: 0.1
    - Number of iterations: 1000
    - Alphas search: 0.0001, 0.0001, 0.001, 0.1 - for grid search params
    - Number of neighbors: 2 - for KNN

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Stock Price Prediction Evaluation
 
-This project is indented to present a small evaluation of different types of regression models for predicting stock prices for [AAPL](https://finance.yahoo.com/quote/AAPL/history?p=AAPL). It is focused on a simple linear, KNN and lasso regression models.
+This project is indented to present a small evaluation of different types of regression models for predicting stock prices for [AAPL](https://finance.yahoo.com/quote/AAPL/history?p=AAPL).
 
 ## Setup
 
@@ -16,31 +16,49 @@ Steps:
 
 | Method | Confidence | Mean Absolute Error | Mean Squared Error | Root Mean Squared Error |
 | :---: | ---: | ---: | ---: | ---: |
-| KNN    | 98.372% |  3.602 |  21.156 |  4.6 |
-| Linear | 97.116% |  9.241 |  114.052 |  10.68 |
-| Lasso  | 97.124% |  9.544 |  121.564 |  11.026 |
+| Linear | 96.54% | 9.267 | 114.54 | 3.044 |
+| KNN | 98.39% | 3.717 | 23.151 | 1.928 |
+| Lasso | 96.55% | 9.567 | 122.002 | 3.093 |
+| Ridge | 96.54% | 9.261 | 114.403 | 3.043 |
+| Ridge CV | 96.54% | 9.261 | 114.403 | 3.043 |
+| Kernel Ridge | 60.8% | 41.406 | 1762.293 | 6.435 |
+| Elastic Net | 96.47% | 8.639 | 100.439 | 2.939 |
+| Elastic Net CV | 96.54% | 9.04 | 109.289 | 3.007 |
+| Bayesian Ridge | 96.54% | 9.253 | 114.217 | 3.042 |
+| Orthogonal Matching Pursuit | 96.4% | 18.776 | 448.374 | 4.333 |
+| Orthogonal Matching Pursuit CV | 96.54% | 9.268 | 114.556 | 3.044 |
 
 #### Moving Average for Adj Close
 
 ![moving-average-for-adj-close](./assets/moving-avergage-for-adj-close.png)
 
-In the next three paragraphs you can find predictions using KNN, Linear, and Lasso regression models.
-
-### KNN regression
-
-Settings:
-- Neighbors: 2
-
-![knn-regresssion](./assets/KNN%20regression.png)
-
-### Linear regression
-
-![linear-regresssion](./assets/Linear%20Regression.png)
-
-### Lasso regression
+In the next paragraphs you can find predictions using selected methods.
 
 Settings:
 - Alpha: 0.1
+- Number of iterations: 1000
+- Alphas search: 0.0001, 0.0001, 0.001, 0.1
+- Number of neighbors: 2
 
-![lasso-regression](./assets/Lasso%20regression.png)
+![](./assets/Bayesian%20Ridge.jpg)
+
+![](./assets/Elastic%20Net.jpg)
+
+![](./assets/Elastic%20Net%20CV.jpg)
+
+![](./assets/Kernel%20Ridge.jpg)
+
+![](./assets/KNN.jpg)
+
+![](./assets/Lasso.jpg)
+
+![](./assets/Linear.jpg)
+
+![](./assets/Orthogonal%20Matching%20Pursuit.jpg)
+
+![](./assets/Orthogonal%20Matching%20Pursuit%20CV.jpg)
+
+![](./assets/Ridge.jpg)
+
+![](./assets/Ridge%20CV.jpg)
 
--- comparison.py
+++ comparison.py
@@ -3,23 +3,25 @@ import pandas as pd
 import math
 import numpy as np
 from sklearn.preprocessing import scale
-from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, RidgeCV, ElasticNet, ElasticNetCV, BayesianRidge, \
+    OrthogonalMatchingPursuit, OrthogonalMatchingPursuitCV
+from sklearn.kernel_ridge import KernelRidge
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 df = pd.read_csv('./data/AAPL.csv', index_col='Date', parse_dates=True)
 
-print('\nPrint loaded data:\n', df.tail())
+# print('\nPrint loaded data:\n', df.tail())
 
-print('\nNull rows count: ', df.isnull().T.any().T.sum())
-print('Null rows: ')
-print(df[df.isnull().T.any().T])
+# print('\nNull rows count: ', df.isnull().T.any().T.sum())
+# print('Null rows: ')
+# print(df[df.isnull().T.any().T])
 
 # Fill missing values using ffill method
 df = df.ffill()
 
-print('\nNull rows count: ', df.isnull().T.any().T.sum())
+# print('\nNull rows count: ', df.isnull().T.any().T.sum())
 
 # === Feature engineering ===
 
@@ -35,22 +37,22 @@ MIN_PERIODS = 30
 df_regularized['Moving Average'] = df_regularized['Adj Close'].rolling(MOVING_AVERAGE_PERIOD,
                                                                        min_periods=MIN_PERIODS).mean()
 df_regularized.dropna(inplace=True)
-print('\nDF Regularized:\n', df_regularized.tail())
+# print('\nDF Regularized:\n', df_regularized.tail())
 
-df_regularized.filter(['HL Pct', 'Pct Change']).plot()
-plt.legend()
-
-df_regularized.filter(['Adj Close', 'Moving Average']).plot()
-plt.legend()
-
-plt.figure()
-df_regularized['Volume'].plot()
-plt.legend()
-plt.show()
+# df_regularized.filter(['HL Pct', 'Pct Change']).plot()
+# plt.legend()
+#
+# df_regularized.filter(['Adj Close', 'Moving Average']).plot()
+# plt.legend()
+#
+# plt.figure()
+# df_regularized['Volume'].plot()
+# plt.legend()
+# plt.show()
 
 # Data for forecast:
 forecast_rows = math.ceil(0.01 * len(df_regularized))
-print('\nTest rows: ', forecast_rows, ' out of: ', len(df_regularized))
+# print('\nTest rows: ', forecast_rows, ' out of: ', len(df_regularized))
 
 FORECASTING_COLUMN = 'Adj Close'
 df_regularized['label'] = df_regularized[FORECASTING_COLUMN].shift(-forecast_rows)
@@ -59,7 +61,7 @@ df_regularized['label'] = df_regularized[FORECASTING_COLUMN].shift(-forecast_row
 df_regularized_forecast = df_regularized.copy()
 
 # Display the head
-print('df_regularized:\n', df_regularized.head())
+# print('df_regularized:\n', df_regularized.head())
 
 # drop - returns all columns except specified ones in the list
 X = np.array(df_regularized.drop(['label'], 'columns'))
@@ -74,192 +76,95 @@ df_regularized.dropna(inplace=True)
 y = np.array(df_regularized['label'])
 
 # Display first 10 Xs (independent variable) and ys (dependent variable)
-print('X: ', X[:40])
-print('y: ', y[:10])
-print('df_regularized:\n', df_regularized.head())
+# print('X: ', X[:40])
+# print('y: ', y[:10])
+# print('df_regularized:\n', df_regularized.head())
 
 # Split data into train & test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1)
 
 # Verify the data
-print('X train: ', X_train.shape, X_train[:10])
-print('is X train finite: ', np.isfinite(X_train).all())
-print('is y train finite: ', np.isfinite(y_train).all())
+# print('X train: ', X_train.shape, X_train[:10])
+# print('is X train finite: ', np.isfinite(X_train).all())
+# print('is y train finite: ', np.isfinite(y_train).all())
 
-#
-# ==== Linear Regression ====
-#
-linear_regression_model = LinearRegression()
-linear_regression_model.fit(X_train, y_train)
+models_summary = []
 
-# Display coefficients
-print('Coef: ', linear_regression_model.coef_)
 
-# Display confidence
-linear_regression_confidence = linear_regression_model.score(X_test, y_test)
+def evaluate_model(model_name, model):
+    model.fit(X_train, y_train)
 
-# Prediction
-y_forecast = linear_regression_model.predict(X_forecast)
+    # Display confidence
+    model_confidence = model.score(X_test, y_test)
 
-# Assign default NaN predictions
-lr_df_regularized_forecast = df_regularized_forecast.copy()
-lr_df_regularized_forecast['Forecast'] = np.nan
+    # Prediction
+    y_forecast = model.predict(X_forecast)
 
-for idx, y_f in enumerate(y_forecast):
-    lr_df_regularized_forecast.iloc[-forecast_rows + idx] = list(
-        lr_df_regularized_forecast.drop('Forecast', 'columns').iloc[-forecast_rows + idx].values) + [y_f]
+    # Assign default NaN predictions
+    model_df_regularized_forecast = df_regularized_forecast.copy()
+    model_df_regularized_forecast['Forecast'] = np.nan
 
-# Display last 200 entries
-plt.figure()
-lr_df_regularized_forecast.iloc[-200:]['Adj Close'].plot()
-lr_df_regularized_forecast.iloc[-200:]['Forecast'].plot()
-plt.title('Linear Regression')
-# plt.show()
+    for idx, y_f in enumerate(y_forecast):
+        model_df_regularized_forecast.iloc[-forecast_rows + idx] = list(
+            model_df_regularized_forecast.drop('Forecast', 'columns').iloc[-forecast_rows + idx].values) + [y_f]
 
-#
-# ==== KNN Regression ====
-#
+    # Add last n entries - forecast rows
+    expected_data = model_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close']
+    forecast_data = model_df_regularized_forecast.iloc[-forecast_rows:]['Forecast']
 
-knn_model = KNeighborsRegressor(n_neighbors=2)
-knn_model.fit(X_train, y_train)
+    model_summary = {
+        'confidence': model_confidence * 100,
+        'expected_values': expected_data,
+        'forecast_values': forecast_data,
+        'mae': mean_absolute_error(expected_data, forecast_data),
+        'mse': mean_squared_error(expected_data, forecast_data),
+        'rmse': np.sqrt(mean_absolute_error(expected_data, forecast_data)),
+        'name': model_name
+    }
 
-# Display confidence
-knn_confidence = knn_model.score(X_test, y_test)
+    return model_summary
 
-# Prediction
-y_forecast = knn_model.predict(X_forecast)
 
-# Assign default NaN predictions
-knn_df_regularized_forecast = df_regularized_forecast.copy()
-knn_df_regularized_forecast['Forecast'] = np.nan
+# Hyperparams
+max_iter = int(1e3)
+alpha = .1
+alphas = [1e-4, 1e-3, 1e-2, 1e-1]
 
-for idx, y_f in enumerate(y_forecast):
-    knn_df_regularized_forecast.iloc[-forecast_rows + idx] = list(
-        knn_df_regularized_forecast.drop('Forecast', 'columns').iloc[-forecast_rows + idx].values) + [y_f]
+models_summary.append(evaluate_model('Linear', LinearRegression()))
 
-# Display last 200 entries
-plt.figure()
-knn_df_regularized_forecast.iloc[-200:]['Adj Close'].plot()
-knn_df_regularized_forecast.iloc[-200:]['Forecast'].plot()
-plt.title('KNN Regression')
+models_summary.append(evaluate_model('KNN', KNeighborsRegressor(n_neighbors=2)))
 
-#
-# ==== Lasso Regression ====
-#
+models_summary.append(evaluate_model('Lasso', Lasso(alpha=alpha, max_iter=max_iter)))
 
-lasso_model = Lasso(alpha=0.1)
-lasso_model.fit(X_train, y_train)
+models_summary.append(evaluate_model('Ridge', Ridge(alpha=alpha, max_iter=max_iter)))
 
-# Display confidence
-lasso_confidence = lasso_model.score(X_test, y_test)
+models_summary.append(evaluate_model('Ridge CV', RidgeCV(alphas=alphas)))
 
-# Prediction
-y_forecast = lasso_model.predict(X_forecast)
+models_summary.append(evaluate_model('Kernel Ridge', KernelRidge(alpha=alpha)))
 
-# Assign default NaN predictions
-lasso_df_regularized_forecast = df_regularized_forecast.copy()
-lasso_df_regularized_forecast['Forecast'] = np.nan
+models_summary.append(evaluate_model('Elastic Net', ElasticNet(alpha=alpha, max_iter=max_iter)))
 
-for idx, y_f in enumerate(y_forecast):
-    lasso_df_regularized_forecast.iloc[-forecast_rows + idx] = list(
-        lasso_df_regularized_forecast.drop('Forecast', 'columns').iloc[-forecast_rows + idx].values) + [y_f]
+models_summary.append(evaluate_model('Elastic Net CV', ElasticNetCV(alphas=alphas, max_iter=max_iter)))
 
-# Display last 200 entries
-plt.figure()
-lasso_df_regularized_forecast.iloc[-200:]['Adj Close'].plot()
-lasso_df_regularized_forecast.iloc[-200:]['Forecast'].plot()
-plt.title('Lasso Regression')
+models_summary.append(evaluate_model('Bayesian Ridge', BayesianRidge(n_iter=max_iter)))
+
+models_summary.append(evaluate_model('Orthogonal Matching Pursuit', OrthogonalMatchingPursuit()))
+
+models_summary.append(evaluate_model('Orthogonal Matching Pursuit CV', OrthogonalMatchingPursuitCV()))
+
+for model_summary in models_summary:
+    print('| {} | {}% | {} | {} | {} |'.format(
+        model_summary['name'],
+        round(model_summary['confidence'], 2),
+        round(model_summary['mae'], 3),
+        round(model_summary['mse'], 3),
+        round(model_summary['rmse'], 3),
+    ))
+
+    plt.figure()
+    plt.title(model_summary['name'])
+    model_summary['expected_values'].plot()
+    model_summary['forecast_values'].plot()
+    plt.savefig('./assets/{}.jpg'.format(model_summary['name']))
+
 plt.show()
-
-print('Confidences: ')
-print('- Linear regression: ', round(linear_regression_confidence * 100., 3), '%')
-print('- KNN regression: ', round(knn_confidence * 100., 3), '%')
-print('- Lasso regression: ', round(lasso_confidence * 100., 3), '%')
-
-print('\nMean Absolute Error:')
-print('- Linear regression: ',
-      round(
-          mean_absolute_error(
-              lr_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              lr_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          ),
-          3
-      ))
-print('- KNN regression: ',
-      round(
-          mean_absolute_error(
-
-              knn_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              knn_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          ),
-          3))
-print('- Lasso regression: ',
-      round(
-          mean_absolute_error(
-              lasso_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              lasso_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          ),
-          3))
-
-print('\nMean Squared Error:')
-print('- Linear regression: ',
-      round(
-          mean_squared_error(
-              lr_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              lr_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          ),
-          3
-      ))
-print('- KNN regression: ',
-      round(
-          mean_squared_error(
-              knn_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              knn_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          ),
-          3))
-print('- Lasso regression: ',
-      round(
-          mean_squared_error(
-              lasso_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              lasso_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          ),
-          3))
-
-print('\nRoot Mean Squared Error:')
-print('- Linear regression: ',
-      round(
-          np.sqrt(mean_squared_error(
-              lr_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              lr_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          )),
-          3
-      ))
-print('- KNN regression: ',
-      round(
-          np.sqrt(mean_squared_error(
-              knn_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              knn_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          )),
-          3))
-print('- Lasso regression: ',
-      round(
-          np.sqrt(mean_squared_error(
-              lasso_df_regularized_forecast.iloc[-forecast_rows:]['Adj Close'],
-              lasso_df_regularized_forecast.iloc[-forecast_rows:]['Forecast'],
-          )),
-          3))
-
-# Mean Absolute Error:
-# - Linear regression:  19.318
-# - KNN regression:  9.581
-# - Lasso regression:  19.406
-#
-# Mean Squared Error:
-# - Linear regression:  472.715
-# - KNN regression:  137.833
-# - Lasso regression:  476.549
-#
-# Root Mean Squared Error:
-# - Linear regression:  21.742
-# - KNN regression:  11.74
-# - Lasso regression:  21.83
